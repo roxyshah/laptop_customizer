7b2f39fbaaf917dc50597a2c3af9074af20e7d2a
Roxanna Shahpouri <roxannaShahpouri@gmail.com>
2019-09-01 13:45:53 -0400

    first commit, separating App component into other components

---
+++ src/FeatureList/FeatureList.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+import slugify from 'slugify';
+import './FeatureList.css';
+
+// This object will allow us to
+// easily convert numbers into US dollar values
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
+
+class FeatureList extends Component {
+    render() {
+        const features = Object.keys(this.props.features).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const options = this.props.features[feature].map(item => {
+              const itemHash = slugify(JSON.stringify(item));
+              return (
+                <div key={itemHash} className="feature__item">
+                  <input
+                    type="radio"
+                    id={itemHash}
+                    className="feature__option"
+                    name={slugify(feature)}
+                    checked={item.name === this.props.selected[feature].name}
+                    onChange={e => this.props.updateFeatureCallback(feature, item)}
+                  />
+                  <label htmlFor={itemHash} className="feature__label">
+                    {item.name} ({USCurrencyFormat.format(item.cost)})
+                  </label>
+                </div>
+              );
+            });
+      
+            return (
+              <fieldset className="feature" key={featureHash}>
+                <legend className="feature__name">
+                  <h3>{feature}</h3>
+                </legend>
+                {options}
+              </fieldset>
+            );
+          });
+
+          return (
+            <form className="main__form">
+              <h2>Customize your laptop</h2>
+              { features }
+            </form>
+          );
+    }
+}
+
+export default FeatureList;
